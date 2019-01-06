4d4cddd69a1a9f9d6f636623394fc7a9c93dc591
dfmabbas <dfmabbas@gmail.com>
2019-01-07 01:08:52 +0330

    fmt shell

--- ellaco.sh
+++ ellaco.sh
@@ -11,28 +11,28 @@ log="log.txt"
 rm -rf $log
 touch $log
 printLOG(){
-	echo $1 >> $log
+    echo $1 >> $log
 }
 
-while getopts q:f:s: option 
-do 
- case "${option}" 
- in 
- q) q=${OPTARG};;
- s) s=${OPTARG};;
- f) f=${OPTARG};;
- esac 
+while getopts q:f:s: option
+do
+    case "${option}"
+        in
+        q) q=${OPTARG};;
+        s) s=${OPTARG};;
+        f) f=${OPTARG};;
+    esac
 done
 
-if [[ $s == *"none"* ]]; then 
-	subtitle=""
-else 
-	subtitle="--write-auto-sub --sub-lang $s --embed-subs"
+if [[ $s == *"none"* ]]; then
+    subtitle=""
+else
+    subtitle="--write-auto-sub --sub-lang $s --embed-subs"
 fi
 
-if [[ $q != *"best"* ]]; then 
-	echo "UNKNOWN ARGUMENT"
-	return
+if [[ $q != *"best"* ]]; then
+    echo "UNKNOWN ARGUMENT"
+    return
 else quality="-f $q"
 fi
 
@@ -46,33 +46,33 @@ playLisQuery="$quality $setting $subtitle $listOutput"
 videoQuery="$quality $setting $subtitle $videoOutput"
 
 downoadURL(){
-	if [[ $1 == *"youtube"* && $1 == *"&list="* ]]; then
-		youtube-dl $playLisQuery $1
-	elif [[ $1 == *"youtube"* && $1 == *"watch"* ]]; then
-		youtube-dl $videoQuery $1
-	elif [[ $1 == *"youtube"* && $1 == *"channel"* ]]; then
-		youtube-dl $channelQuery $1
-	elif [[ $1 == *"http"* && $1 != *"youtube"* ]]; then
-		aria2c $ariaQuery $1
-	else 
-		printLOG "JUMP"
-		return
-	fi
-
-	printLOG "DOWNLOADED URL -> $1"
+    if [[ $1 == *"youtube"* && $1 == *"&list="* ]]; then
+        youtube-dl $playLisQuery $1
+        elif [[ $1 == *"youtube"* && $1 == *"watch"* ]]; then
+        youtube-dl $videoQuery $1
+        elif [[ $1 == *"youtube"* && $1 == *"channel"* ]]; then
+        youtube-dl $channelQuery $1
+        elif [[ $1 == *"http"* && $1 != *"youtube"* ]]; then
+        aria2c $ariaQuery $1
+    else
+        printLOG "JUMP"
+        return
+    fi
+    
+    printLOG "DOWNLOADED URL -> $1"
 }
 
 exec < $f
- while read -r url
-  do
-
-   if [[ $1 == *"end"* ]]; then
-		#say complete download items #macos
-		echo "COMPLETE DOWNLOAD ITEMS"
-		break
-		return
-	else downoadURL $url
-	fi
-	
-  done
+while read -r url
+do
+    
+    if [[ $1 == *"end"* ]]; then
+        #say complete download items #macos
+        echo "COMPLETE DOWNLOAD ITEMS"
+        break
+        return
+    else downoadURL $url
+    fi
+    
+done
 exec < "$terminal"
