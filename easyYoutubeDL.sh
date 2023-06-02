06e63611d2e2bf2c9dbcb1320199f2140a344615
Abbas Naqdi <naqdi.ab@gmail.com>
2023-06-02 07:23:17 +0330

    replace yt-dlp instade of youtube-dl for improve multiple connection download & bugfix

--- easyYoutubeDL.sh
+++ easyYoutubeDL.sh
@@ -5,21 +5,22 @@ clear
 OS=uname
 echo "Processing ..."
 
-q="highest"
+q="1080"
 s="none"
 p=$PWD
-m="2g"
+m="6g"
 t="video"
+fps="60"
 
 while getopts :t:q:m:p:s:f: option; do
 	case "${option}" in
-		t) t=${OPTARG} ;;
-q) q=${OPTARG} ;;
-m) m=${OPTARG} ;;
-p) p=${OPTARG} ;;
-s) s=${OPTARG} ;;
-f) f=${OPTARG} ;;
-esac
+	t) t=${OPTARG} ;;
+	q) q=${OPTARG} ;;
+	m) m=${OPTARG} ;;
+	p) p=${OPTARG} ;;
+	s) s=${OPTARG} ;;
+	f) f=${OPTARG} ;;
+	esac
 done
 
 log="$p/log.txt"
@@ -29,52 +30,48 @@ printLOG() {
 	echo $1 >>$log
 }
 
-setting="--console-title -ciw -4 -R infinite --max-filesize $m"
+setting="--compat-options all --external-downloader aria2c -N 8 --console-title -ciw -4 -R infinite --max-filesize $m"
 
 if [[ $s == *"none"* ]]; then
 	subtitle=""
 else
 	subtitle="--write-sub --write-auto-sub --embed-subs --sub-lang $s --convert-subs srt"
 fi
-
-quality="-f bestvideo[ext=mp4]+bestaudio/bestvideo+bestaudio/best"
+quality="-f bestvideo[height=$q][fps=$fps]+bestaudio/bestvideo[height<=$q][fps<=$fps]+bestaudio/bestvideo+bestaudio/best --merge-output-format mp4"
 
 if [[ $t == *"audio"* ]]; then
-	quality="-f bestaudio[ext=m4a]"
-elif [[ $q != *"highest"* ]]; then
-	quality="-f $q"
+	quality="-f bestaudio/best --merge-output-format m4a"
 fi
-
 
 videoOutput="-o $p/youtube/other/%(title)s.%(ext)s"
 listOutput="-o $p/youtube/%(playlist)s/%(playlist_index)s.%(title)s.%(ext)s"
-channelOutput="-o $p/youtube/%(uploader)s/(channel)s/(playlist)s/%(playlist_index)s.%(title)s.%(ext)s"
+channelOutput="-o $p/youtube/%(uploader)s/%(playlist)s/%(playlist_index)s.%(title)s.%(ext)s"
 musicOutput="-o $p/soundcloud/%(playlist)s/%(playlist_index)s.%(title)s.%(ext)s"
 otherOutput="-o $p/other/%(playlist)s/%(playlist_index)s.%(title)s.%(ext)s"
 hlsOutput="-o $p/hls/%(title)s.%(ext)s"
 
-video="$quality $setting $subtitle $videoOutput"
-playlist="$quality $setting $subtitle $listOutput"
-channel="$quality $setting $subtitle $channelOutput"
+video="$setting $quality $subtitle $videoOutput"
+playlist="$setting $quality $subtitle $listOutput"
+channel="$setting $quality $subtitle $channelOutput"
 music="$setting $musicOutput"
 other="$setting $quality $subtitle $otherOutput"
 hlsConfig="$setting $quality $subtitle $hlsOutput"
 
 downoadURL() {
 	if [[ $t == *"audio"* ]] && [[ $1 == *"soundcloud"* ]]; then
-		youtube-dl  $music -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -k 2M -x 16 -s 16 -c -j 4 -m 60 --retry-wait=6' $1
+		yt-dlp $music -i $1
 	elif [[ $t == *"video"* ]]; then
 		if [[ $1 == *"youtube"* ]] && [[ $1 == *"watch"* ]]; then
-			youtube-dl  $video -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -k 2M -x 16 -s 16 -c -j 4 -m 60 --retry-wait=6' $1
+			yt-dlp $video --no-playlist -i $1
+		elif [[ $1 == *"youtube"* ]] && [[ $1 == *"@"* || $1 == *"user"* ]]; then
+			yt-dlp $channel -i $1
 		elif [[ $1 == *"youtube"* ]] && [[ $1 == *"list"* ]]; then
-			youtube-dl  $playlist -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -k 2M -x 16 -s 16 -c -j 4 -m 60 --retry-wait=6' $1
-		elif [[ $1 == *"youtube"* ]] && [[ $1 == *"channel"* || $1 == *"user"* ]]; then
-			youtube-dl  $channel -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -k 2M -x 16 -s 16 -c -j 4 -m 60 --retry-wait=6' $1
+			yt-dlp $playlist -i $1
 		fi
 	elif [[ $t == *"hls"* ]]; then
-		youtube-dl --hls-use-mpegts $hlsConfig $1
-	else 
-		youtube-dl $other -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -k 2M -x 16 -s 16 -c -j 4 -m 60 --retry-wait=6' $1
+		yt-dlp --hls-use-mpegts $hlsConfig $1 &
+	else
+		yt-dlp $other -i $1
 	fi
 
 	printLOG "complete -> $1 & next ..."
@@ -86,14 +83,15 @@ while read -r url; do
 		printLOG "jump"
 		echo "complete download items"
 		if [[ $OS == "Darwin" ]]; then
-            say complete download items #macos
-        fi
-        break
-        return
-    elif [[ $t == *"hls"* ]]; then
-    	downoadURL $url
-    else downoadURL $url
-    fi
-    
+			say complete download items #macos
+		fi
+		break
+		return
+	elif [[ $t == *"hls"* ]]; then
+		downoadURL $url
+	else
+		downoadURL $url
+	fi
+
 done
 exec <"$terminal"
